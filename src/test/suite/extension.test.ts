e8bbd454e9694b72b92b4f0059505241506ec2ce
Kevin Rose <kevin+gh@maypark.com>
2020-05-09 18:46:07 -0500

    update tests to use mocked setting so it works in cicd

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,7 +3,7 @@ import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+// import * as vscode from 'vscode';
 import * as indent from '../../indent';
 
 suite("dedent current line", function () {
@@ -175,30 +175,6 @@ suite("dedent current line", function () {
             ], 2));
     });
 });
-suite("trim whitespace-only lines", function () {
-    const trimConfig = "trimLinesWithOnlyWhitespace";
-    let settings = vscode.workspace.getConfiguration("pythonIndent");
-    test("do not trim whitespace from empty line by default", async () => {
-        await settings.update(trimConfig, settings.inspect(trimConfig)!.defaultValue);
-        assert.equal(false, indent.trimCurrentLine("    "));
-    });
-    test("do not trim whitespace from non-empty line by default", function () {
-        assert.equal(false, indent.trimCurrentLine("    a"));
-    });
-    test("do not trim whitespace from non-empty line if configured", async () => {
-        await settings.update(trimConfig, true);
-        assert.equal(false, indent.trimCurrentLine("    a"));
-    });
-    test("trim whitespace on empty line if configured", function () {
-        assert.equal(true, indent.trimCurrentLine("    "));
-    });
-    test("trim whitespace on empty line with tabs if configured", function () {
-        assert.equal(true, indent.trimCurrentLine("    \t"));
-    });
-    test("unset trim config settings", async () => {
-        await settings.update(trimConfig, undefined);
-    });
-});
 
 suite("extend comment line", function () {
     test("should extend", function () {
@@ -224,5 +200,57 @@ suite("extend comment line", function () {
             "  # x    ", 6));
         assert.equal(false, indent.extendCommentToNextLine(
             "  # x    ", 7));
+    });
+});
+
+class MockedFalseWorkspaceConfiguration {
+    trimLinesWithOnlyWhitespace = false;
+    get(section: string) {
+        return undefined;
+    }
+    has(section: string) {
+        return true;
+    }
+    inspect(section: string) {
+        return undefined;
+    }
+    async update(section: string, value: boolean) {
+        return undefined;
+    }
+}
+
+class MockedTrueWorkspaceConfiguration {
+    trimLinesWithOnlyWhitespace = true;
+    get(section: string) {
+        return undefined;
+    }
+    has(section: string) {
+        return true;
+    }
+    inspect(section: string) {
+        return undefined;
+    }
+    async update(section: string, value: boolean) {
+        return undefined;
+    }
+}
+
+suite("trim whitespace-only lines", function () {
+    const falseSetting = new MockedFalseWorkspaceConfiguration();
+    const trueSetting = new MockedTrueWorkspaceConfiguration();
+    test("do not trim whitespace from empty line without setting", async () => {
+        assert.equal(false, indent.trimCurrentLine("    ", falseSetting));
+    });
+    test("do not trim whitespace from non-empty line without setting", function () {
+        assert.equal(false, indent.trimCurrentLine("    a", falseSetting));
+    });
+    test("do not trim whitespace from non-empty line with setting", async () => {
+        assert.equal(false, indent.trimCurrentLine("    a", trueSetting));
+    });
+    test("trim whitespace on empty line with setting", function () {
+        assert.equal(true, indent.trimCurrentLine("    ", trueSetting));
+    });
+    test("trim whitespace on empty line with tabs with setting", function () {
+        assert.equal(true, indent.trimCurrentLine("    \t", trueSetting));
     });
 });
